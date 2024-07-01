15034c945ca400d0b38568abfd6eeacd79f2906a
Manish Sijwali <manishsijwali@gmail.com>
2024-07-02 00:32:36 +0530

    css

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TodoApp' // Replace 'your-repo-name' with the actual name of your GitHub repository
+  base: '/todoApp' // Replace 'your-repo-name' with the actual name of your GitHub repository
 });
